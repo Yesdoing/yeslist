e84e4eca96c991c9970705a78321b502a84931a1
Jane Lee <Yesdoing13@gmail.com>
2018-07-15 21:22:43 +0900

    [js] ToDo 클래스 생성 및 initialize 함수 생성

---
+++ js/ToDo.js
@@ -0,0 +1,37 @@
+export class ToDo {
+  constructor(todo, startDate, endDate, checked) {
+    Object.assign(this, { todo, startDate, endDate, checked });
+  }
+
+  get todo() {
+    return this._todo;
+  }
+
+  set todo(value) {
+    this._todo = value;
+  }
+
+  get startDate() {
+    return this._startDate;
+  }
+
+  set startDate(value) {
+    this._startDAte = value;
+  }
+
+  get endDate() {
+    return this._endDate;
+  }
+
+  set endDate(value) {
+    this._endDate = value;
+  }
+
+  get checked() {
+    return this._checked;
+  }
+
+  set checked(value) {
+    this._checked = value;
+  }
+}
---
+++ js/app.js
@@ -0,0 +1,42 @@
+import ToDo from "./ToDo";
+
+function intialize() {
+  let todos = [];
+
+  let a = new ToDo("1번할일", new Date(), new Date(), false);
+  todos.unshift(a);
+  let b = new ToDo("2번할일", new Date(), new Date(), false);
+  todos.unshift(b);
+  let c = new ToDo("3번할일", new Date(), new Date(), false);
+  todos.unshift(c);
+  todos.unshift(new ToDo("4번할일", new Date(), new Date(), false));
+
+  const ul = document.querySelector("ul");
+  const form = document.querySelector("form");
+  const btn = document.querySelector("button");
+  const text = document.querySelector("input");
+
+  todos.forEach(value => {
+    let li = document.createElement("li");
+    li.textContent = value.todo;
+    ul.appendChild(li);
+  });
+
+  document.addEventListener("keyup", e => {
+    if (e.keyCode === 13) {
+      let str = text.value;
+      let todoItem = new ToDo(str, new Date(), new Date(), false);
+      todos.unshift(todoItem);
+      createTodoItem(todoItem);
+      text.value = "";
+    }
+  });
+
+  function createTodoItem(todoItem) {
+    let li = document.createElement("li");
+    li.textContent = todoItem.todo;
+    ul.appendChild(li);
+  }
+}
+
+intialize();
